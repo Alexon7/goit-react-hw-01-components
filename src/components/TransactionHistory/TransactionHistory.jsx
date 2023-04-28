b108315979c642fa6772a570993b0f0bed45fcd8
Alexon7 <alenkaalexon@gmail.com>
2023-04-28 17:08:21 +0200

    task -04

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(transaction => (
+          <tr
+            key={transaction.id}
+            className={
+              (transaction.type === 'payment' && css.payment) ||
+              (transaction.type === 'invoice' && css.invoice) ||
+              (transaction.type === 'deposit' && css.deposit) ||
+              (transaction.type === 'withdrawal' && css.withdrawal)
+            }
+          >
+            <td>{transaction.type}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
