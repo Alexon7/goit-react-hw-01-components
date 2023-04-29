99d468d24175982508c7c0e3f22775519b1126cb
Alexon7 <alenkaalexon@gmail.com>
2023-04-29 11:18:08 +0200

    fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -8,7 +8,7 @@ export const Statistics = ({ title, stats }) => {
       {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
-        {stats.map(({ id, label, percentage } = stats, index) => (
+        {stats.map(({ id, label, percentage } = stats) => (
           <li
             className={css.item}
             style={{ backgroundColor: getRandomHexColor() }}
